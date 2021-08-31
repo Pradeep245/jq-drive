e31b495a401ee6f2eb62cdcc58d84da0fd1dc7ab
Pradeep245 <65214831+Pradeep245@users.noreply.github.com>
2021-08-31 11:25:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 5000;
+// const helmet = require('helmet');
+ const cors = require('cors')
+ app.use(cors())
+ const path = require('path')
+app.use( express.static(path.join(__dirname, 'public')))
+ 
+// app.use(
+// 	helmet({
+// 	  contentSecurityPolicy: {
+// 		directives: {
+// 		  ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+// 		  "script-src": ["'self'", "'nonce-2726c7f26c'"],
+// 		},
+// 	  },
+// 	})
+//   );
+
+
+const multer = require('multer')
+const { google } = require('googleapis')
+
+const fs = require('fs')
+//const unlinkAsync = promisify(fs.unlink)
+
+app.set('view engine', 'ejs')
+const dir = './upload'
+const CLIENT_ID = '813472167072-7hk1afdthisdji8qjsmq1m1poulu5umg.apps.googleusercontent.com'
+const CLIENT_SECRET = 'Tqy_7qDpc1eSqJ_cdjL0k4MR'
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+const REFRESH_TOKEN = '1//04H_m5VQ95vjJCgYIARAAGAQSNwF-L9Ir6OLxMNc917NxYNUpb0NeXfl1q8Du4lYQAd2X5a6yJS5lA84mDpcqT8sHtG9pzONvCtw'
+
+const oauth2Client = new google.auth.OAuth2(
+	CLIENT_ID,
+	CLIENT_SECRET,REDIRECT_URI
+
+	);
+
+oauth2Client.setCredentials({refresh_token:REFRESH_TOKEN})
+
+const drive = google.drive({
+	version:'v3',
+	auth:oauth2Client
+})
+
+app.get('/home', function (req, res) {
+
+	res.sendFile(path.join(__dirname+'/index.html'));
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+})
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+callback(null, './upload')
+    
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname)
+  }
+});
+
+var upload = multer({ storage: storage });
+
+app.post('/projects', upload.array('uploadedImages', 10), async function(req, res, err) {
+  if (err) {
+   // console.log('error');
+    console.log(err);
+  }
+
+  var filess = await req.files;
+await res.json({"files":[filess]});
+
+await setTimeout(datas , 10000 )
+  
+ // await datas();
+
+
+
+
+ 
+});
+
+
+
+// 
+
+
+
+
+ function datas(){
+	const files = fs.readdirSync(dir)
+	console.log(dir);
+var dirs = []
+for (const file of files) {
+  dirs.push(`./upload/${file}`)
+
+}
+ async function uploadss() {
+	try{
+		for(link in dirs){
+			let liss = `${dirs[link]}`;
+
+			console.log(typeof liss)
+			if (liss.includes('.jpeg') || liss.includes('.jpg')) {
+		const response = await drive.files.create({
+			requestBody: {
+				name : `one${link}.jpg`,
+				mimeType:'image/jpg'
+			},
+			media:{
+				mimeType:'image/jpg',
+				body: fs.createReadStream(dirs[link]),
+			},
+		})
+		
+		console.log(response.data)
+}
+else if(liss.includes('.pdf')){
+	let response = await drive.files.create({
+			requestBody: {
+				name : `one${link}.pdf`,
+				mimeType:'application/pdf'
+			},
+			media:{
+				mimeType:'application/pdf',
+				body: fs.createReadStream(dirs[link]),
+}
+})
+
+	console.log(response.data.id)
+	}
+}
+	}catch(error){
+		console.log(error)
+	}
+
+
+}
+
+uploadss();
+}
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+	console.log(`port ${PORT} started`)
+})
---
+++ public/js/demo.js
@@ -0,0 +1,173 @@
+
+// // $(function() {
+// //   'use strict';
+
+// //   // Initialize the jQuery File Upload widget:
+// //   $('#fileupload').fileupload({
+// //     // Uncomment the following to send cross-domain cookies:
+// //     //xhrFields: {withCredentials: true},
+// //     url: ''
+// //   });
+
+//   // Enable iframe cross-domain access via redirect option:
+//   // $('#fileupload').fileupload(
+//   //   'option',
+//   //   'redirect',
+//   //   window.location.href.replace(/\/[^/]*$/, '/cors/result.html?%s')
+//   // );
+
+//   if (window.location.hostname === 'blueimp.github.io') {
+//     // // Demo settings:
+//     // $('#fileupload').fileupload('option', {
+//     //   url: '',
+//     //   // Enable image resizing, except for Android and Opera,
+//     //   // which actually support image resizing, but fail to
+//     //   // send Blob objects via XHR requests:
+//     //   disableImageResize: /Android(?!.*Chrome)|Opera/.test(
+//     //     window.navigator.userAgent
+//     //   ),
+//     //   maxFileSize: 99900000,
+//     //   acceptFileTypes: /(\.|\/)(gif|jpe?g|png|pdf)$/i
+//     // });
+//     // // Upload server status check for browsers with CORS support:
+//     // if ($.support.cors) {
+//     //   $.ajax({
+//     //     url: '//jquery-file-upload.appspot.com/',
+//     //     type: 'HEAD'
+//     //   }).fail(function() {
+//     //     $('<div class="alert alert-danger"/>')
+//     //       .text('Upload server currently unavailable - ' + new Date())
+//     //       .appendTo('#fileupload');
+//     //   });
+//     // }
+//   } else {
+//     // Load existing files:
+//     // $('#fileupload').addClass('fileupload-processing');
+//     // $.ajax({
+//     //   // Uncomment the following to send cross-domain cookies:
+//     //   xhrFields: {withCredentials: true},
+//     //   url: $('#fileupload').fileupload('option', 'url'),
+//     //   dataType: 'json',
+//     //   context: $('#fileupload')[0]
+//     // })
+//     //   .always(function() {
+//     //     $(this).removeClass('fileupload-processing');
+//     //   })
+//     //   .done(function(result) {
+//     //     $(this)
+//     //       .fileupload('option', 'done')
+//     //       // eslint-disable-next-line new-cap
+//     //       .call(this, $.Event('done'), { result: result });
+//     //   });
+//   }
+// });
+// $(document).ready(function() { 
+//   var router = new AppRouter();
+
+//   $('#fileupload').fileupload({
+//       url: '',
+      
+//       //acceptFileTypes: /(\.|\/)(tar|tgz|tar.gz)$/i,
+//       //maxFileSize: 20000000,  // 20 MB
+  
+//       add: function (e, data) {
+//           var acceptFileTypes = /^application\/(tar|tgz|tar.gz|gzip|x-tar|pdf|jpg|jpeg)$/i;
+//           alert(data.files[0]['type']); // Pour vérifier la syntaxe de l'extension
+//           //alert(data.files[0]['size']); Pour vérifier la taille du fichier
+//           if(data.files[0]['type'].length && !acceptFileTypes.test(data.files[0]['type'])) { //si le type ne correspond pas à ceux définits dans acceptFileTypes
+//           $('#info').append('<font color=red><b>Mauvaise extension</b></font><br/><br/>Extensions valides : <b>(tar|tgz|tar.gz)</b>');
+  
+//           }
+//           else if(data.files[0]['size'] > 20000000) { //si la taille du fichier dépasse 20 MB
+//           $('#info').append('<font color=red><b>La taille du fichier doit etre inférieure à 20 MO</b></font>');   
+  
+//           }
+//           else {
+//             data.preventDefault();
+//           var jqXHR = data.submit(); //upload le fichier direct après qu'on l'ai choisi
+//           }
+//       },
+//       progress: function(e, data){
+//           var progress = parseInt(data.loaded / data.total * 100, 10); // calcule l'avancement de l'upload et l'affiche dans la bar progress
+//           $('#progress .progress-bar').css(
+//           'width',
+//           progress + '%'
+//           );
+//           if (progress == 100) {
+//           $('#info').append('<font color=green><b>Fichier uploadé !</b></font>');
+//           }
+//       }
+  
+//   });
+//   $('#fileupload').fileupload(
+//         'option',
+//         'redirect',
+//         window.location.href.replace(/\/[^/]*$/, '/cors/result.html?%s')
+//       );
+  
+//   });
+$(function() {
+
+'use strict';
+ 
+// Initialize the jQuery File Upload widget:
+$('#fileupload').fileupload({
+    // Uncomment the following to send cross-domain cookies:
+    //xhrFields: {withCredentials: true},
+    url: 'http://localhost:5000/projects'
+    //console.log('ggggg')
+});
+
+// Enable iframe cross-domain access via redirect option:
+$('#fileupload').fileupload(
+    'option',
+    'redirect',
+    window.location.href.replace(
+        /\/[^\/]*$/,
+        '/cors/result.html?%s'
+    )
+);
+
+if (window.location.hostname === '127.0.0.1') {
+    // $('#fileupload').fileupload('option', {
+    //     url: 'http://localhost:5000/projects',
+    //     // Enable image resizing, except for Android and Opera,
+    //     // which actually support image resizing, but fail to
+    //     // send Blob objects via XHR requests:
+    //     disableImageResize: /Android(?!.*Chrome)|Opera/
+    //         .test(window.navigator.userAgent),
+    //     //maxFileSize: 999000,
+    //     acceptFileTypes: /(\.|\/)(gif|jpe?g|png|pdf)$/i
+    // });
+    // Upload server status check for browsers with CORS support:
+    if ($.support.cors) {
+        // console.log(window.location.hostname);
+        // $.ajax({
+        //     url: 'http://localhost:5000/projects',
+        //     type: 'POST'
+        // }).fail(function () {
+        //     $('<div class="alert alert-danger"/>')
+        //         .text('Upload server currently unavailable - ' +
+        //                 new Date())
+        //         .appendTo('#fileupload');
+        // });
+    }
+} else {
+    // Load existing files:
+    console.log(window.location.hostname);
+    $('#fileupload').addClass('fileupload-processing');
+    $.ajax({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        context: $('#fileupload')[0]
+    }).always(function () {
+        $(this).removeClass('fileupload-processing');
+    }).done(function (result) {
+        $(this).fileupload('option', 'done')
+            .call(this, $.Event('done'), {result: result});
+    });
+}
+
+});
